dc40fbe7b5129ca22212e67ffd1bd0370a27f806
Mudassir Ahmed Mohammed <mudassir.mohammed@mavs.uta.edu>
2016-05-24 02:47:26 -0500

    Add files via upload

---
+++ HourGlass.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class HourGlass{
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for(int i=0; i < 6; i++){
+            for(int j=0; j < 6; j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int sum = 0;
+        int temp=-999;
+         for(int i=0; i < 4; i++){
+            for(int j=i;j<4; j++){
+                sum+=arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2];
+               
+                
+            }
+              if(sum>temp)
+              {
+                temp=sum;   
+             }
+             
+        }
+        System.out.println(temp);
+       
+    }
+}
